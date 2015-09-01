5e6ba246a17701a9c9733d4e196416380effa010
phealeyh <tranceformas_101@hotmail.com>
2015-09-01 18:56:22 +1000

    Finished Exercise5

--- exercise5/app/src/main/java/exercise5/id11723222/com/exercise5/MainActivity.java
+++ exercise5/app/src/main/java/exercise5/id11723222/com/exercise5/MainActivity.java
@@ -1,48 +1,58 @@
 package exercise5.id11723222.com.exercise5;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mLeftView, mMiddleView, mRightView;
+public class MainActivity extends AppCompatActivity{
+    private ListView mList_item;
+    private TrainAdapter mAdapter;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_item);
-        setUpViews();
-        //data model
+        setContentView(R.layout.activity_main);
+        mList_item = (ListView) findViewById(R.id.list_view);
+        mContext = this;
         ArrayList<TrainData> trainList = new ArrayList<TrainData>();
-        //set the array up
         setUpTrains(trainList);
         //an adapter is a binding between the data source and the given views
-        TrainAdapter adapter = new TrainAdapter(this,trainList);
+        mAdapter = new TrainAdapter(this,trainList);
+        mList_item.setAdapter(mAdapter);
+        listenForListItem();
         //use onItemClickListener to setup the interaction between a view and the user
 
     }
 
-    /**Function:setUpViews
-     * --------------------
-     * This function will set up the views by linking them with their
-     * corresponding views within the xml. Bring xml to java.
-     */
-
-    private void setUpViews(){
-        //list views are responsible for taking the data from a value from an adapter
-        //and responsible for displaying the actual data from that adapter
-        mLeftView = (ListView) findViewById(R.id.left_pane);
-        mMiddleView = (ListView) findViewById(R.id.middle_pane);
-        mRightView = (ListView) findViewById(R.id.right_pane);
 
 
+
+    private void listenForListItem(){
+        mList_item.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //make the time refresh for that specific row
+                mAdapter.refreshTime(position);
+            }
+        });
     }
+
 
 
 
@@ -70,7 +80,68 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        else if(id == R.id.add){
+            mAdapter.addData();
+            return true;
+
+        }
+        else if(id == R.id.refresh){
+            //hide list view
+            mList_item.setVisibility(View.INVISIBLE);
+            //show progressbar for 3 seconds
+            new RefreshProgress().execute();
+            //show changed arrival times
+            mAdapter.generateNewTimes();
+        }
+
+        else if(id == R.id.delete_all){
+            mAdapter.deleteAll();
+            return true;
+
+        }
+        else if(id == R.id.quit){
+            //quit the application
+            finish();
+            return true;
+
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class RefreshProgress extends AsyncTask<Void, Void, Void> {
+        private ProgressDialog progressDialog;
+
+        public RefreshProgress(){
+            //initialise progress dialog
+            progressDialog = new ProgressDialog(mContext);
+        }
+
+        @Override
+        protected void onPreExecute(){
+            //show progress dialog
+            progressDialog.setMessage(getResources().getString(R.string.refreshing));
+            progressDialog.show();
+            progressDialog.setIndeterminate(true);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params){
+            try {
+                //pause for 3 seconds
+                Thread.sleep(3000L);
+            } catch (InterruptedException e1){
+                e1.printStackTrace();
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void nothing){
+            progressDialog.dismiss();
+            mList_item.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
--- exercise5/app/src/main/java/exercise5/id11723222/com/exercise5/TrainAdapter.java
+++ exercise5/app/src/main/java/exercise5/id11723222/com/exercise5/TrainAdapter.java
@@ -1,12 +1,24 @@
 package exercise5.id11723222.com.exercise5;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by phealeyhang on 25/08/15.
@@ -15,9 +27,39 @@ public class TrainAdapter extends BaseAdapter{
 
     private ArrayList<TrainData> trainList;
     private View view;
+    private Context mContext;
 
-    public TrainAdapter(Context context, ArrayList trainList){
+    public TrainAdapter(Context context, ArrayList<TrainData> trainList){
+        mContext = context;
         this.trainList = trainList;
+    }
+
+    public void addData(){
+        trainList.add(new TrainData("South West", 4, "late", "Canley Vale", "4:40"));
+        notifyDataSetChanged();
+    }
+
+    public void deleteAll(){
+        //remove all elements within the container
+        trainList.removeAll(trainList);
+        notifyDataSetChanged();
+    }
+
+    public void refreshTime(int position){
+        //refresh time based on position
+        for(int i = 0; i < trainList.size(); i++){
+            if(trainList.get(i) == trainList.get(position)){
+                trainList.get(i).setmArrivalTime(new Random().nextInt(20) + 1);
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    public void generateNewTimes(){
+        for(TrainData train: trainList) {
+            train.setmArrivalTime(new Random().nextInt(20) + 1);
+        }
+        notifyDataSetChanged();
     }
 
     @Override
@@ -38,23 +80,54 @@ public class TrainAdapter extends BaseAdapter{
     /**
      *
      * Function: getView
-     * -----------------
+     * -------------------
      * This will get a selected view from the given data source
      */
 
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        //return a view by generating a view or recycling it
+        TextView arriving_in_time, platform_name, arrival_time, status, destination_time,
+                destination_location;
         View view;
+        //helps with performance
         if(convertView != null){
             view = convertView;
         }
+        //parse a new view from xml
         else{
-            //build view from a given xml
-            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,null);
+            //The LayoutInflater takes your layout XML-files
+            //and creates different View-objects from its contents.
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, null);
         }
+        //fill in view with data
+        arriving_in_time = (TextView) view.findViewById(R.id.arrival_time_text);
+        arriving_in_time.setText(Integer.toString(trainList.get(position).getmArrivalTime())
+        + " min");
+        platform_name = (TextView) view.findViewById(R.id.platform_text);
+        platform_name.setText(trainList.get(position).getmPlatform());
+        arrival_time = (TextView) view.findViewById(R.id.arrival_text);
+        arrival_time.setText(trainList.get(position).getmDestinationTime());
+        arrival_time.setTextColor(Color.BLACK);
+        status = (TextView) view.findViewById(R.id.status_text);
+        status.setText(trainList.get(position).getmStatus());
+        if(trainList.get(position).getmStatus().equals("on-time")){
+            status.setTextColor(Color.GREEN);
+        }
+        else{
+            status.setTextColor(Color.RED);
+        }
+        destination_time = (TextView) view.findViewById(R.id.destination_time_text);
+        destination_time.setText(trainList.get(position).getmDestinationTime());
+        destination_time.setTextColor(Color.BLUE);
+        destination_location = (TextView) view.findViewById(R.id.destination_location);
+        destination_location.setText(trainList.get(position).getmDestination());
+        destination_location.setTypeface(null, Typeface.BOLD);
         return view;
     }
+
+
+
+
 
 }
